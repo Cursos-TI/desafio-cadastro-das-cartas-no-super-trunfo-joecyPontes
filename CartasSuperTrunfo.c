772157e99ab68edaf7de9114707c97de5993ce58
Joecy Pontes <142790869+joecyPontes@users.noreply.github.com>
2025-08-11 21:23:24 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,24 +7,14 @@ int main() {
     char codigoCarta[3]; // Letra do estado + número
     char estado;
     char nomeCidade[50];
-    int populacao;
+    unsigned int populacao;
     float areaKm2;
     float pib;
     int pontosTuristicos;
     float densidadePopulacional;
+    float inversoDensidade;
     float pidperCapita;
-
-    
-    char codigoCarta1[3]; // Letra do estado + número
-    char estado1;
-    char nomeCidade1[50];
-    int populacao1;
-    float areaKm21;
-    float pib1;
-    int pontosTuristicos1;
-    float densidadePopulacional1;
-    float pidperCapita1;
-    
+    long double superpoder;
     // Cadastro
     printf("Cadastro da primeira carta:\n");
     
@@ -51,12 +41,23 @@ int main() {
     
     densidadePopulacional = (float) populacao / (float) areaKm2;
     pidperCapita = (float) pib / (float) populacao;
-    
-    
- 
-    printf("\nCadastro da segunda carta:\n");
-    
-    
+    inversoDensidade = (float) areaKm2 / (float) populacao;
+    superpoder = populacao + areaKm2 + pib + pontosTuristicos + inversoDensidade + densidadePopulacional + pidperCapita;
+
+
+    char codigoCarta1[3]; // Letra do estado + número
+    char estado1;
+    char nomeCidade1[50];
+    unsigned int populacao1;
+    float areaKm21;
+    float pib1;
+    int pontosTuristicos1;
+    float densidadePopulacional1;
+    float inversoDensidade1;
+    float pidperCapita1;
+    long double superpoder1;
+    // Cadastro
+    printf("Cadastro da primeira carta:\n");
     
     printf("Digite a primeira letra do Estado (A-H):\n");
     scanf(" %c", &estado1);
@@ -78,12 +79,12 @@ int main() {
     
     printf("Digite o número de pontos turísticos:\n");
     scanf(" %d", &pontosTuristicos1);
+    
+    densidadePopulacional1 = (float) populacao1 / (float) areaKm21;
+    pidperCapita1 = (float) pib1 / (float) populacao1;
+    inversoDensidade1 = (float) areaKm21 / (float) populacao1;
+    superpoder1 = populacao1 + areaKm21 + pib1 + pontosTuristicos1 + inversoDensidade1 + densidadePopulacional1 + pidperCapita1;
 
-    densidadePopulacional1 = (float) populacao1 / areaKm21;
-    pidperCapita1 = pib1 / (float)populacao1;
-    
-    
-    
     // Exibição dos dados
     
     printf("\nCarta: %s\n", codigoCarta);
@@ -102,8 +103,10 @@ int main() {
     printf("Densidade Populacional: %.2f hab/km²\n", densidadePopulacional);
 
     printf("PIB per Capita: %.2f\n", pidperCapita);
-    
 
+    printf("superpoder: %.2d\n", superpoder);
+
+//
     printf("\nCarta: %s\n", codigoCarta1);
 
     printf("Estado: %c\n", estado1);
@@ -121,5 +124,22 @@ int main() {
 
     printf("PIB per Capita: %.2f\n", pidperCapita1);
 
+    printf("superpoder: %.2d\n", superpoder1);
+    
 
+
+
+
+    // comparar
+    printf("Para sabermos os vencedores vamos dizer que %s é '1' e %s é '0'\n", codigoCarta, codigoCarta1);
+   
+    printf("no quesito população o vencedor é: %d\n", populacao> populacao1);
+    printf("no quesito área em km² o vencedor é: %d\n", areaKm2 > areaKm21);
+    printf("no quesito PIB o vencedor é: %d\n", pib > pib1);
+    printf("no quesito pontos turísticos o vencedor é: %d\n", pontosTuristicos > pontosTuristicos1);
+    printf("no quesito densidade populacional o vencedor é: %d\n", densidadePopulacional < densidadePopulacional1);
+    printf("no quesito PIB per Capita o vencedor é: %d\n", pidperCapita > pidperCapita1);
+    printf("no quesito superpoder o vencedor é: %d\n", superpoder > superpoder1);
+    return 0;
+ 
 }
